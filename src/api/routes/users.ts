047ebc8ba9cab9b553e90d5fa52dee86bc5734f6
Gin <ginderick@gmail.com>
2023-06-07 20:58:22 +0800

    add a post users route to add a user

--- src/api/routes/users.ts
+++ src/api/routes/users.ts
@@ -3,14 +3,27 @@ import { NextFunction, Request, Response, Router } from 'express';
 
 const route = Router();
 
+// Create an instance of the UsersService class to persist in-memory store
+const usersService = new UsersService();
+
 const users = (app: Router) => {
   app.use('/users', route);
 
   route.get('/', async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const usersService = new UsersService();
       const users = await usersService.getUsers();
       return res.status(200).json(users);
+    } catch (error) {}
+  });
+
+  route.post('/', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      console.log(req.body);
+      const user = req.body.user as string;
+      await usersService.addUser(user);
+      return res
+        .status(201)
+        .json({ message: 'User has been successfully added' });
     } catch (error) {}
   });
 };
