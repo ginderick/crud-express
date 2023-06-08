93460035f72285fdd8709b4bb5253f570783df4d
Gin <ginderick@gmail.com>
2023-06-08 10:14:20 +0800

    update usersList and update correct type

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,30 +1,41 @@
+import { User } from '../types';
+
 export default class UsersService {
-  private usersList: string[];
+  private usersList: User[];
 
   constructor() {
     this.usersList = [
-      'Harper Sullivan',
-      'Leo Thompson',
-      'Ava Reynolds',
-      'Owen Mitchell',
-      'Stella Foster',
-      'Caleb Hayes',
-      'Ruby Porter',
-      'Isaac Morgan',
-      'Grace Ramirez',
-      'Elijah Cooper',
-      'Mia Peterson',
-      'Ethan Brooks',
-      'Olivia Price',
-      'Noah Campbell',
-      'Sophia Turner',
+      {
+        id: 1,
+        name: 'Harper Sullivan',
+        age: 20,
+        email: 'harpersullivan@gmail.com',
+      },
+      {
+        id: 2,
+        name: 'Leo Thompson',
+        age: 18,
+        email: 'leothompson@gmail.com',
+      },
+      {
+        id: 3,
+        name: 'Owen Mitchell',
+        age: 22,
+        email: 'owenmitchell@gmail.com',
+      },
+      {
+        id: 4,
+        name: 'Ava Reynolds',
+        age: 28,
+        email: 'avareynolds@gmail.com',
+      },
     ];
   }
   public async getUsers() {
     return this.usersList;
   }
 
-  public async addUser(user: string) {
+  public async addUser(user: User) {
     this.usersList.push(user);
     return;
   }
