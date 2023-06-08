76fd1f6829ab342d0fb429e497cf29efe2fc4cfb
Gin <ginderick@gmail.com>
2023-06-08 17:10:21 +0800

    add users service test cases

--- tests/unit/services/users.test.ts
+++ tests/unit/services/users.test.ts
@@ -19,4 +19,45 @@ describe('User service', () => {
 
     expect(users.length).toEqual(seedData.length);
   });
+
+  it('should return get a user with id 1', async () => {
+    const usersService = new UsersService();
+    const id = 1;
+    const user = await usersService.getUser(id);
+
+    expect(user!.name).toEqual(seedData[0].name);
+  });
+
+  it('should return an updated user details with id 1', async () => {
+    const usersService = new UsersService();
+    const id = 1;
+    const data = {
+      name: 'Gin Derick Magno',
+      age: 22,
+      email: 'ginderick@gmail.com',
+    };
+    const user = await usersService.updateUser(id, data);
+
+    expect(user!.name).toEqual(data.name);
+  });
+
+  it('should return the created user', async () => {
+    const usersService = new UsersService();
+    const data = {
+      name: 'Gin Derick Magno',
+      age: 22,
+      email: 'ginderick@gmail.com',
+    };
+    const user = await usersService.addUser(data);
+
+    expect(user!.name).toEqual(data.name);
+  });
+
+  it('should return the deleted user with id 1', async () => {
+    const usersService = new UsersService();
+    const id = 1;
+    const userId = await usersService.deleteUser(id);
+
+    expect(userId).toEqual(id);
+  });
 });
