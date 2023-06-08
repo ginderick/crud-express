685e0639f1ba0f7db29178e0c7e5e4e15e0ca775
Gin <ginderick@gmail.com>
2023-06-08 11:01:20 +0800

    remove usersList in the user service and update service functions using database calls

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,42 +1,20 @@
+import { InMemoryDatabase } from '../database';
 import { User } from '../types';
 
 export default class UsersService {
-  private usersList: User[];
-
-  constructor() {
-    this.usersList = [
-      {
-        id: 1,
-        name: 'Harper Sullivan',
-        age: 20,
-        email: 'harpersullivan@gmail.com',
-      },
-      {
-        id: 2,
-        name: 'Leo Thompson',
-        age: 18,
-        email: 'leothompson@gmail.com',
-      },
-      {
-        id: 3,
-        name: 'Owen Mitchell',
-        age: 22,
-        email: 'owenmitchell@gmail.com',
-      },
-      {
-        id: 4,
-        name: 'Ava Reynolds',
-        age: 28,
-        email: 'avareynolds@gmail.com',
-      },
-    ];
-  }
   public async getUsers() {
-    return this.usersList;
+    const database = InMemoryDatabase.getInstance();
+    return database.getAll();
   }
 
   public async addUser(user: User) {
-    this.usersList.push(user);
-    return;
+    const database = InMemoryDatabase.getInstance();
+    const userResult = await database.create(user);
+    return userResult;
+  }
+
+  public async getUser() {
+    const database = InMemoryDatabase.getInstance();
+    return database.getUser('Gin Derick');
   }
 }
